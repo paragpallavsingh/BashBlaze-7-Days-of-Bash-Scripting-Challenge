170347e2a9fa88d979817b5ee184df724d40b97f
Parag Pallav Singh <thakurparag6.1@gamil.com>
2023-08-02 10:15:51 +0530

    conflict resolved day 2

--- Challenges/Day_2/explorer.sh
+++ Challenges/Day_2/explorer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 printf "Day 2 : Script Part 1 : explorer.sh\n"
 printf "\nTask 1: Print all files, directories one time, with size in human readable format\n"
 ls -lh | awk '{printf " - %s (%s) \n",$9,$5}'
@@ -32,26 +31,3 @@ do
 	fi
 done
 printf "BBye for now! #twsbashblazechallenge rocks\n"
-=======
-# Part 1: File and Directory Exploration
-echo "Welcome to the Interactive File and Directory Explorer!"
-
-while true; do
-    # List all files and directories in the current path
-    echo "Files and Directories in the Current Path:"
-    ls -lh
-
-    # Part 2: Character Counting
-    read -p "Enter a line of text (Press Enter without text to exit): " input
-
-    # Exit if the user enters an empty string
-    if [ -z "$input" ]; then
-        echo "Exiting the Interactive Explorer. Goodbye!"
-        break
-    fi
-
-    # Calculate and print the character count for the input line
-    char_count=$(echo -n "$input" | wc -m)
-    echo "Character Count: $char_count"
-done
->>>>>>> d838192f38ba443f63e003f94915dc7618887f6b
